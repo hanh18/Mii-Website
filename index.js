14fbc82da60ce78cec16aa702e0e4bbea88d79cb
hanh18 <htmhanh19@gmail.com>
2022-07-05 16:56:15 +0700

    config graphql

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ import {} from 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import methodOverride from 'method-override';
+import { ApolloServer } from 'apollo-server-express';
+
+import mongoDataMethods from './data/query';
+// Load schema & resolve
+import typeDefs from './schema/schema';
+import resolvers from './resolver/resolver';
 
 const app = express();
 // eslint-disable-next-line no-underscore-dangle
@@ -22,6 +28,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // override using a query value
 app.use(methodOverride('_method'));
+
+let apolloServer = null;
+
+// context: store function access by resolvers
+// eslint-disable-next-line consistent-return
+async function startServer() {
+  try {
+    apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: (request) => ({
+        request,
+        mongoDataMethods,
+      }),
+    });
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app });
+  } catch (error) {
+    return console.log(error);
+  }
+}
+
+startServer();
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hey', message: 'Hello there!' });
