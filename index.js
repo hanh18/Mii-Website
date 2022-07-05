00385ab9d4d6423eb29fdbf48483e7bbcbca03d5
hanh18 <htmhanh19@gmail.com>
2022-07-05 13:57:38 +0700

    setup es6 and npm basic

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 import express from 'express'
+import path from 'path'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import {} from 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import methodOverride from 'method-override';
+
 const app = express()
-const port = 3000
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// override using a query value
+app.use(methodOverride("_method"));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
 
+const port = 3000
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
   console.log(`http://localhost:${port}`)
 })
