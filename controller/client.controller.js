795b49ee73ccb39e8a838d36cd4e8343a2db0425
hanh18 <htmhanh19@gmail.com>
2022-07-14 16:42:03 +0700

    done API change avatar

--- controller/client.controller.js
+++ controller/client.controller.js
@@ -20,47 +20,12 @@ const changeAvata = async (req, res) => {
     // get id from cookies
     const id = parseInt(req.body.id, 10);
 
-    // console.log(req.file)
-    // eslint-disable-next-line camelcase
-    // let publicId;
-    // const cld_upload_stream = await cloudinary.uploader.upload_stream(
-    //   {
-    //     folder: 'Miii',
-    //   },
-    //   function (error, result) {
-    //     console.log(error, result);
-    //     publicId = result.public_id;
-    //     console.log(publicId)
-    //   }
-    // );
-    // console.log(cld_upload_stream)
-
-    // await streamifier.createReadStream(req.file.buffer).pipe(cld_upload_stream);
-    // console.log(publicId)
-    // await cloudinary.uploader.destroy('Miii/v6lkbuqonfw0y30t3fp6', { resource_type: 'image', invalidate: true, type: 'upload' }, (error, result) => { console.log(result, error); });
-
-    
-
-    // console.log(result)
-    // const publicId = result.public_id
-    // console.log(publicId)
-
     if (!req.file) {
       console.log('no file');
       return arrResponse.badRequest(res, arrMessage.MESSAGE_REQUIRED_IMAGE);
     }
 
-    // const avatarPath = req.file.path;
-
-    // await prisma.user.update({
-    //   where: { id },
-    //   data: {
-    //     avatar: avatarPath,
-    //   },
-    // });
-
-    // cloudinary.api.delete_resources(publicId, function(error, result){console.log(result);});
-    
+    // upload image
     const uploadFromBuffer = () => new Promise((resolve, reject) => {
       const cldUploadStream = cloudinary.uploader.upload_stream(
         {
@@ -76,24 +41,40 @@ const changeAvata = async (req, res) => {
       );
       streamifier.createReadStream(req.file.buffer).pipe(cldUploadStream);
     });
-   
+
     const resultUpload = await uploadFromBuffer();
     const publicId = resultUpload.public_id;
+    const avatarPath = resultUpload.url;
 
-    await cloudinary.uploader.destroy(
-      publicId,
-      {
-        resource_type: 'image',
-        invalidate: true,
-        type: 'upload',
-      },
-      (error, result) => {
-        if (error) {
-          console.log(error);
-          return arrResponse.error(res, arrMessage.MESSAGE_DELETE_IMAGE_ERROR);
-        }
-      },
-    );
+    // update user
+    // eslint-disable-next-line no-inner-declarations
+    async function runUpdate() {
+      await prisma.user.update({
+        where: { id },
+        data: {
+          avatar: avatarPath,
+        },
+      });
+    }
+
+    runUpdate()
+      .catch(async (e) => {
+        await cloudinary.uploader.destroy(
+          publicId,
+          {
+            resource_type: 'image',
+            invalidate: true,
+            type: 'upload',
+          },
+          (error, result) => {
+            if (error) {
+              console.log(error);
+            }
+          },
+        );
+
+        throw e;
+      });
 
     arrResponse.success(res);
   } catch (error) {
